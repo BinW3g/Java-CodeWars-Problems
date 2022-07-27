8fb51edc4d99db2345d2eb47fe9a0467e39ea7fe
Florian Angerer <florian.angerer.2001@gmail.com>
2022-07-27 14:04:56 +0200

    Simple Pig Latin Done

---
+++ src/main/java/codewars/at/SimplePigLatin/PigLatin.java
@@ -0,0 +1,24 @@
+package codewars.at.SimplePigLatin;
+
+import java.util.Arrays;
+import java.util.List;
+
+
+//Link to codewars problem:
+//https://www.codewars.com/kata/520b9d2ad5c005041100000f
+public class PigLatin {
+    public static String pigIt(String str) {
+        String[] tokens = str.split(" ");
+        String out = "";
+        List punctuations = Arrays.asList("!",".","?");
+        for (String token : tokens){
+            if(punctuations.contains(token)){
+                out += token + " ";
+            }else{
+                out += token.substring(1) + token.charAt(0) + "ay ";
+            }
+        }
+        System.out.printf(out);
+        return out.substring(0, out.length()-1);
+    }
+}
